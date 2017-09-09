d9d75dfeabe44d86181006b912f99da9cde1b923
SereneAngelina <sereneangelina1@gmail.com>
2017-09-10 00:58:30 +0200

    Delete song of the artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,4 +7,6 @@ class ArtistsController < ApplicationController
   def show
     @artist = Artist.find(params[:id])
   end
+
+  
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,6 +8,20 @@ def create
   redirect_to @new_song.artist
 end
 
+
+
+  def destroy
+
+    @artist = Artist.find(params[:artist_id])
+    @song = @artist.songs.find(params[:id])
+
+    @song.destroy
+    redirect_to artist_path(@artist)
+
+
+  end
+
+
 private
 def playlist_params
     params.require(:song).permit(:song_name, :song_url, :artist_id)
