5d944fab2772ed9de1460e3eebaa5bfaad8f1955
SereneAngelina <sereneangelina1@gmail.com>
2017-09-09 20:10:47 +0200

    Add new song to artist

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,16 @@
 class SongsController < ApplicationController
-  
+
+def create
+
+  @new_song = Song.new(playlist_params)
+  @new_song.artist_id = params[:artist_id]
+  @new_song.save
+  redirect_to @new_song.artist
+end
+
+private
+def playlist_params
+    params.require(:song).permit(:song_name, :song_url, :artist_id)
+  end
+
 end
