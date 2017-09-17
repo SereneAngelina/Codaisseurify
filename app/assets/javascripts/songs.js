dff479496ca5376ed0c7aa7b697ab4c554ec33d0
SereneAngelina <sereneangelina1@gmail.com>
2017-09-17 16:15:40 +0200

    Add delete API

---
+++ app/assets/javascripts/songs.js
@@ -0,0 +1,12 @@
+// function deleteSong(artistId, songId) {
+//   $.ajax({
+//     type: "DELETE",
+//     url: "/artists/" + artistId + "/songs/" + songId,
+//     contentType: "application/json",
+//     dataType: "json"
+//   })
+//   .done(function(data) {
+//     alert('Song deleted successfully!')
+//     location.reload();
+//   });
+// }
