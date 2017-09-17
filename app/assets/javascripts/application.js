69d160568da510125b738f929b44644b985dfcb0
SereneAngelina <sereneangelina1@gmail.com>
2017-09-17 15:57:27 +0200

    Add delete song api

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,15 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require_tree .
+function deleteSong(artistId, songId) {
+  $.ajax({
+    type: "DELETE",
+    url: "/api/artists/" + artistId + "/songs/" + songId+ ".json",
+    contentType: "application/json",
+    dataType: "json"
+  })
+  .done(function(data) {
+    alert('Song deleted successfully!')
+    location.reload();
+  });
+}
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,23 +1,42 @@
 class SongsController < ApplicationController
 
 
+# POST /song
+ # POST /song.json
 def create
    @new_song = Song.new(playlist_params)
    @new_song.artist_id = params[:artist_id]
-   if @new_song.save
-   redirect_to @new_song.artist
-   else
-     redirect_to @new_song.artist, notice: "Please fill valid details"
+     respond_to do |format|
+       if @new_song.save
+         format.html { redirect_to @new_song.artist }
+         format.json { render :create, status: :created }
+       else
+         format.html { redirect_to @new_song.artist,  notice: 'Please enter valid details' }
+         format.json { render json: @new_song.errors, status: :unprocessable_entity }
+       end
+     end
    end
-
-end
+# def create
+#    @new_song = Song.new(playlist_params)
+#    @new_song.artist_id = params[:artist_id]
+#    if @new_song.save
+#    redirect_to @new_song.artist
+#    else
+#      redirect_to @new_song.artist, notice: "Please fill valid details"
+#    end
+#
+# end
 
 
 def destroy
     @artist = Artist.find(params[:artist_id])
     @song = @artist.songs.find(params[:id])
     @song.destroy
-    redirect_to artist_path(@artist)
+    respond_to do |format|
+       format.html { redirect_to artist_path(@artist) }
+        format.json { header :no_content }
+     end
+
 end
 
 
